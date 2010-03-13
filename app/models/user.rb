1d0132adc34591168f4c89b74b32d7edb9719c92
Daniel Hahn <ghub@limitedcreativity.org>
2010-03-13 21:01:00 +0100

    Validations and simple unit test

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ActiveRecord::Base
   has_many :friendships
   has_many :friends, :through => :friendships
   
+  validates_presence_of :name
+  validates_uniqueness_of :name
+  
 end
--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,8 +1,36 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  
+  def test_friendship
+    assert_equal([users(:amy)], users(:bob).friends)
   end
+  
+  def test_no_name
+    foo = User.new
+    assert_raises(ActiveRecord::RecordInvalid) { foo.save! }
+  end
+  
+  def test_duplicate_name
+    amy2 = User.new
+    amy2.name = "Amy"
+    assert_raises(ActiveRecord::RecordInvalid) { amy2.save! }
+  end
+  
+  def test_duplicate_friend
+    steve = User.new
+    steve.name = "Steve"
+    steve.friends << users(:amy)
+    steve.friends << users(:amy)
+    assert_raises(ActiveRecord::RecordInvalid) { steve.save! }
+  end
+  
+  def test_assign_user
+    thelma = User.new
+    thelma.name = "Thelma"
+    thelma.friends << users(:amy)
+    thelma.save!
+    assert_equal([users(:amy)], thelma.friends)
+  end
+  
 end
