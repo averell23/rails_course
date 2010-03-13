125b4532fa77ecdaeb32d7eb42bbf7e7e4ef93d6
Daniel Hahn <ghub@limitedcreativity.org>
2010-03-13 21:47:00 +0100

    script/generate controller user_sessions -> User acts_as_authentic -> Added route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 ActionController::Routing::Routes.draw do |map|
   map.resources :users
+  map.resource :user_session
 
   # The priority is based upon order of creation: first created -> highest priority.
 
